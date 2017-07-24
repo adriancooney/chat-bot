d7e714af2592a36d05270f3cff6045e629d42efe
Adrian Cooney <cooney.adrian@gmail.com>
2017-07-24 16:39:41 +0100

    Fixing Mention rule

--- src/rules/Mention.js
+++ src/rules/Mention.js
@@ -1,16 +1,28 @@
 import Rule from "../Rule";
 
 export default class Mention extends Rule {
+    constructor(props, context) {
+        super(props, context);
+
+        if(!context.service && !props.handle && !props.symbol) {
+            throw new Error("Mention cannot implicitly match a mention to the current user without a service context.");
+        }
+    }
+
     async match(input) {
         let formatted;
 
         if(this.props.handle) {
-            formatted = `${this.props.symbol}${this.props.handle}`;
+            if(this.props.symbol) {
+                formatted = `${this.props.symbol}${this.props.handle}`;
+            } else {
+                formatted = this.context.service.formatMention({ handle: this.props.handle });
+            }
         } else {
             formatted = this.context.service.formatMention(await this.context.service.getCurrentUser(input.community));
         }
 
-        const match = input.content.match(new RegExp(`^\\s*${formatted}\\s+`, "i"));
+        const match = input.content.match(new RegExp(`^\\s*${formatted}(\\s+|$)`, "i"));
 
         if(match) {
             return {
